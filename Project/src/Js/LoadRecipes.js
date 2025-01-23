9912fd605d0f736ca6798a96fa8a539239ad7fed
Gabriel Ferreira <gabferreira983@gmail.com>
2025-01-23 15:45:18 +0000

    Dar set up a index.html para receber dados apenas por localstorage para não gastar calls da api (150 por dia)

--- Project/src/Js/LoadRecipes.js
+++ Project/src/Js/LoadRecipes.js
@@ -1,8 +1,10 @@
+import { spoonacularApiConfig } from "./ApiConfig.js";
+import { RecipeElement } from "./LocalStorageRecipes.js";
 
 
 async function FetchRecipes() {
   try {
-    const response = await fetch(`${urlRecipeUnique}?apiKey=${recipeKeyUnique}&number=10&offset=0`);
+    const response = await fetch(`${spoonacularApiConfig.url}?apiKey=${spoonacularApiConfig.apiKey}&number=10&offset=0`);
     const data = await response.json();
 
     if (response.status !== 200) {
@@ -10,7 +12,7 @@ async function FetchRecipes() {
       return { results: [], offset: 0, number: 0, totalResults: 0 }; // Return a default value in case of error
     }
 
-    console.log(data);
+    console.log(data.results);
 
     return data.results;
 
@@ -19,3 +21,5 @@ async function FetchRecipes() {
     return { results: [], offset: 0, number: 0, totalResults: 0 }; // Return a default value in case of error
   }
 }
+
+
